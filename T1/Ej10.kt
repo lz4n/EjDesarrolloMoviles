6255fa84e9bb40398a922decdeba92ff5a2c3e98
IzanPerezDímas <izan.perez.dimas@gmail.com>
2023-10-20 12:33:46 +0200

    20-10-2023

--- T1/Ej10.kt
+++ T1/Ej10.kt
@@ -1,11 +1,15 @@
 package T1
 
 fun main() {
-    var num: Int
+    var num = -1
 
     do {
         print("Introduce un numero (mayor o igual a 0): ")
-        num = readln().toInt()
+        try {
+            num = readln().toInt()
+        } catch (x: NumberFormatException) {
+            print("No es un número.")
+        }
     } while (num < 0)
     println("Gracias!")
 }
